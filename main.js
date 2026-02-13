7109e3a644b10e450cdd7b18518a6ed08a85a3c0
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-02-13 13:19:10 +0900

    db

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 import { main } from "./main/game.js"
 
-function resize() {
-  canvas.width  = window.innerWidth
-  canvas.height = window.innerHeight
-}
-
-resize()
-window.addEventListener("resize", resize)
-
 function loop() {
   requestAnimationFrame(loop);
   main()
